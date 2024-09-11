9ea6783a72880ec6f508924d41b1379ff46dd5ea
IKreike <iris.kreike@hva.nl>
2024-09-11 14:51:03 +0200

    V6- red
    red- fullscreen and rotated video back

--- script.js
+++ script.js
@@ -33,12 +33,12 @@ function waiting() {
     video.load();
 }
 
-// function fullscreen() {
-//     if (video.requestFullscreen) {
-//         video.requestFullscreen();
-//     } else if (video.webkitRequestFullscreen) { /* Safari */
-//         video.webkitRequestFullscreen();
-//     } else if (video.msRequestFullscreen) { /* IE11 */
-//         video.msRequestFullscreen();
-//     }
-// }
+function fullscreen() {
+    if (video.requestFullscreen) {
+        video.requestFullscreen();
+    } else if (video.webkitRequestFullscreen) { /* Safari */
+        video.webkitRequestFullscreen();
+    } else if (video.msRequestFullscreen) { /* IE11 */
+        video.msRequestFullscreen();
+    }
+}
