30fa2ed245deeab8ec38e7c5a06234990a5ab5df
IKreike <iris.kreike@hva.nl>
2024-09-13 10:13:58 +0200

    FINAL version

--- script.js
+++ script.js
@@ -12,16 +12,16 @@ function openDoor() {
     removeEventListener('click', openDoor);
     console.log("door opened");
     console.log(sourceVideo.src);
-    sourceVideo.src = "assets/loopV1/Part_2.mp4";
+    sourceVideo.src = "assets/Part_2.mp4";
     console.log(sourceVideo.src);
     video.load();
-    setTimeout(introduction, 25000);
+    setTimeout(introduction, 31000);
 }
 
 function introduction() {
     console.log("door opened");
     console.log(sourceVideo.src);
-    sourceVideo.src = "assets/loopV1/Part_1.mp4";
+    sourceVideo.src = "assets/Part_1.mp4";
     console.log(sourceVideo.src);
     video.load();
     setTimeout(waiting, 9000)
@@ -29,7 +29,7 @@ function introduction() {
 
 function waiting() {
     addEventListener('click', openDoor);
-    sourceVideo.src = "assets/loopV1/Loop.mp4";
+    sourceVideo.src = "assets/Loop.mp4";
     video.load();
 }
 
